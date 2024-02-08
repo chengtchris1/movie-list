efd878bde25c9fe8d004f29b488ba5d05fcc7dfd
Chris Cheng <chengtchris@gmail.com>
2024-02-07 17:31:54 -0800

    Progress on L3

---
+++ client/src/components/ToggleWatchedView.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ToggleWatchedView = (props) => (
+  <div id="watchedToggleArea">
+   <button class="watchedButton" type="button">Watched</button>
+   <button class="toWatchButton" type="button">To watch</button>
+  </div>
+)
+export default SearchView
